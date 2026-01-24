fc7cdda57808b10a7c5b57d98938e7d24dda2175
josephdelgado57-cpu <josephdelgado57@gmail.com>
2026-01-24 13:08:31 -0800

    HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
+// https://www.w3schools.com/java/java_syntax.asp
 public class HelloWorld {
     public static void main(String[] args) {
         // Concept: String output
         System.out.println("Hello World!");
     }
 }
+
