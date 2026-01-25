b8bea2677c39f8378e04dfab58f2284d25506cfa
josephdelgado57-cpu <josephdelgado57@gmail.com>
2026-01-24 16:30:29 -0800

    Update CircleArea.java

--- CircleArea.java
+++ CircleArea.java
@@ -5,14 +5,12 @@ public class CircleArea {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        //Step 1: Prompt for the radius
         System.out.print("Enter the radius of the circle: ");
         double radius = input.nextDouble();
 
-        //Step 2: Caculate the area (Area = PI * r * r)
         double area = Math.PI * radius * radius;
 
-        //Step 3: Display the result
         System.out.println("The area of the circle with radius " + radius + " is " + area);
     }
 }
+
